f9b20d6781745c8ef1cc4493f47e3d1895d1ce4a
Rafal Šurpicki <rafal.surpicki@insoft.lt>
2022-05-04 16:40:56 +0300

    t02 update

--- src/main/java/com/test/scripts/testScripts/TestScripts.java
+++ src/main/java/com/test/scripts/testScripts/TestScripts.java
@@ -22,25 +22,25 @@ public class TestScripts extends TestCase {
 		testScriptsPage = new TestScriptsPage();
 	}
 
-
 	@Test
 	@Description("Checking text in testing article")
 	public void t01() throws Exception {
 		t.get(Constants.DEFAULT_PORTAL_URL);
-		t.setText("Enter text to search bar",TestScriptsPage.searchFieldBy, 5, "testing");
+		t.setText("Enter text to search bar", TestScriptsPage.searchFieldBy, 5, "testing");
 		t.click("Click search button", TestScriptsPage.searchButtonBy, 1);
 		t.click("Click Essay link", TestScriptsPage.essayLinkBy, 2);
 		t.checkIfExists("Check if text exists", TestScriptsPage.essayTextBy, 2);
 	}
-	
+
 	@Test
 	@Description("Count how many references are in Math article")
 	public void t02() throws Exception {
+		int expectedCount = 70;
 		t.get(Constants.DEFAULT_PORTAL_URL);
-		t.setText("Enter text to search bar",TestScriptsPage.searchFieldBy, 5, "maths");
+		t.setText("Enter text to search bar", TestScriptsPage.searchFieldBy, 5, "maths");
 		t.click("Click search button", TestScriptsPage.searchButtonBy, 1);
-		int count = t.findElements(TestScriptsPage.referencesCountBy, 2).size();
-		Assert.assertEquals("Count of references has not changed", 72, count);
+		int count = t.findAll("Find all references", TestScriptsPage.referencesCountBy, 2).size();
+		Assert.assertEquals("Count of references changed", expectedCount, count);
 	}
 
 }
