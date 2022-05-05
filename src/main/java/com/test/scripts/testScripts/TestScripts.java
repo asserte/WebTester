608443926847f97132cfa94ec7059450460675b0
dovilas.bikus <dovilas.bikus@DOVILASBIKUS.x.insoft.lt>
2022-05-05 16:51:03 +0300

    added allure property file

--- src/main/java/com/test/scripts/testScripts/TestScripts.java
+++ src/main/java/com/test/scripts/testScripts/TestScripts.java
@@ -32,9 +32,30 @@ public class TestScripts extends TestCase {
 		t.checkIfExists("Check if text exists", TestScriptsPage.essayTextBy, 2);
 	}
 
-//	@Test
+	@Test
 	@Description("Count how many references are in Math article")
 	public void t02() throws Exception {
+		int expectedCount = 70;
+		t.get(Constants.DEFAULT_PORTAL_URL);
+		t.setText("Enter text to search bar", TestScriptsPage.searchFieldBy, 5, "maths");
+		t.click("Click search button", TestScriptsPage.searchButtonBy, 1);
+		int count = t.findAll("Find all references", TestScriptsPage.referencesCountBy, 2).size();
+		Assert.assertEquals("Count of references changed", expectedCount, count);
+	}
+	
+	@Test
+	@Description("Checking text in testing article")
+	public void t03() throws Exception {
+		t.get(Constants.DEFAULT_PORTAL_URL);
+		t.setText("Enter text to search bar", TestScriptsPage.searchFieldBy, 5, "testing");
+		t.click("Click search button", TestScriptsPage.searchButtonBy, 1);
+		t.click("Click Essay link", TestScriptsPage.essayLinkBy, 2);
+		t.checkIfExists("Check if text exists", TestScriptsPage.essayTextBy, 2);
+	}
+
+	@Test
+	@Description("Count how many references are in Math article")
+	public void t04() throws Exception {
 		int expectedCount = 70;
 		t.get(Constants.DEFAULT_PORTAL_URL);
 		t.setText("Enter text to search bar", TestScriptsPage.searchFieldBy, 5, "maths");
